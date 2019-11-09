6d46c9aacbefebfffbafd6150cffcef80b8e5b59
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-11-09 10:34:03 -0300

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-
-type Features = {
-  id: string
-  name: string
-  description: string
-  votes: {}[]
-}
-
-type FeaturesResponse = {
-  features: Features[]
-}
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import FeaturesPage from './FeaturesPage'
 
 const App: React.FC = () => {
-  const [features, setFeatures] = useState<Features[]>()
-
-  useEffect(() => {
-    axios
-      .get<FeaturesResponse>('/api/features')
-      .then(response => setFeatures(response.data.features))
-  }, [])
-
   return (
-    <>
-      <h1>My Features</h1>
-      {!features && <div>Loading...</div>}
-      {features &&
-        features.map(feature => (
-          <div key={feature.id}>
-            <h3>{feature.name}</h3>
-            <p>{feature.description}</p>
-            <p>Votes: {feature.votes.length}</p>
-          </div>
-        ))}
-    </>
+    <Router>
+      <Switch>
+        <Route path="/" component={FeaturesPage}></Route>
+      </Switch>
+    </Router>
   )
 }
 
