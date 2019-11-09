6691b810e099feff44e1cd13660b990bf1986307
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-11-09 11:14:55 -0300

    Change FeaturePage to be HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import FeaturesPage from './FeaturesPage'
+import HomePage from './HomePage'
 import NewFeaturesPage from './NewFeaturePage'
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={FeaturesPage}></Route>
+        <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/features/new" component={NewFeaturesPage}></Route>
       </Switch>
     </Router>
