380b5588a2701620679e1df813083c3ca3bc0e5b
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-12-04 20:41:25 -0300

    Add option to add all feature in a survey

--- src/NewSurveyPage.tsx
+++ src/NewSurveyPage.tsx
@@ -64,41 +64,55 @@ const NewSurveyPage: React.FC = () => {
 
           {features ? (
             <>
-              <div className="flex max-w-xl mb-2">
-                <div className="relative flex-1">
-                  <select
-                    className="appearance-none bg-white px-3 py-2 block w-full rounded border border-solid border-gray-300 outline-none"
-                    value={selectedFeatureId}
-                    onChange={e => setSelectedFeatureId(e.currentTarget.value)}
-                  >
-                    <option value="">Select a feature</option>
-                    {availableFeatures.map(feature => (
-                      <option value={feature.id} key={feature.id}>
-                        {feature.name}
-                      </option>
-                    ))}
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg
-                      className="fill-current h-4 w-4"
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                    </svg>
-                  </div>
-                </div>
+              {availableFeatures.length > 0 && (
+                <div className="flex max-w-xl mb-2">
+                  <div className="relative flex-1">
+                    <select
+                      className="appearance-none bg-white px-3 py-2 block w-full rounded border border-solid border-gray-300 outline-none"
+                      value={selectedFeatureId}
+                      onChange={e => {
+                        const value = e.currentTarget.value
 
-                <button
-                  className="bg-indigo-100 text-indigo-700 px-3 py-2 rounded-r focus:outline-none text-xs font-medium uppercase"
-                  type="button"
-                  onClick={() =>
-                    setSelectedFeatureIds(ids => [...ids, selectedFeatureId])
-                  }
-                >
-                  Add feature
-                </button>
-              </div>
+                        if (value === 'all') {
+                          setSelectedFeatureIds(
+                            features.map(feature => feature.id)
+                          )
+                          return
+                        }
+
+                        setSelectedFeatureId(value)
+                      }}
+                    >
+                      <option value="">Select a feature</option>
+                      <option value="all">All features</option>
+                      {availableFeatures.map(feature => (
+                        <option value={feature.id} key={feature.id}>
+                          {feature.name}
+                        </option>
+                      ))}
+                    </select>
+                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                      <svg
+                        className="fill-current h-4 w-4"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                      </svg>
+                    </div>
+                  </div>
+
+                  <button
+                    className="bg-indigo-100 text-indigo-700 px-3 py-2 rounded-r focus:outline-none text-xs font-medium uppercase"
+                    type="button"
+                    onClick={() =>
+                      setSelectedFeatureIds(ids => [...ids, selectedFeatureId])
+                    }
+                  >
+                    Add feature
+                  </button>
+                </div>
+              )}
 
               {selectedFeatureIds.map((id, index) => {
                 const selectedFeature = features.find(
