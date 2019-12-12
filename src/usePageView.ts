010c22ba09a2e5ca8fd4eb0ccaba851f85b4ce1b
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-12-11 21:32:54 -0300

    Only send page view on production

--- src/usePageView.ts
+++ src/usePageView.ts
@@ -6,7 +6,7 @@ const usePageView = () => {
   const location = useLocation()
 
   useEffect(() => {
-    if (process.env.REACT_APP_GA_ID) {
+    if (process.env.REACT_APP_GA_ID && process.env.NODE_ENV === 'production') {
       ReactGA.pageview(location.pathname + location.search)
     }
   }, [location.pathname, location.search])
