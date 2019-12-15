1e6bfcab835c34e263e11e370cac3a48ef994d3b
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-12-14 23:45:05 -0300

    Refact new survey page

--- src/NewSurveyPage.tsx
+++ src/NewSurveyPage.tsx
@@ -40,129 +40,136 @@ const NewSurveyPage: React.FC = () => {
         <title>New Survey - Feedbacky</title>
       </Helmet>
 
-      <h1 className="uppercase text-sm font-medium text-gray-700 mb-4">
-        New survey
-      </h1>
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-3 max-w-xl">
-          <label
-            htmlFor=""
-            className="block font-medium text-sm text-gray-700 mb-2"
-          >
-            Name
-          </label>
-          <input
-            required
-            autoFocus
-            name="name"
-            type="text"
-            ref={register}
-            className="bg-white px-3 py-2 block w-full rounded border border-solid border-gray-300 outline-none"
-          />
+      <section className="py-6 bg-white border-t-1 border-indigo-700 shadow mb-4">
+        <div className="container mx-auto px-2 md:flex">
+          <h1 className="leading-tight text-2xl font-medium">New survey</h1>
         </div>
+      </section>
 
-        <div className="mb-3">
-          <label
-            htmlFor=""
-            className="block font-medium text-sm text-gray-700 mb-2"
-          >
-            Features
-          </label>
+      <div className="container mx-auto py-4 px-2">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-3 max-w-xl">
+            <label
+              htmlFor=""
+              className="block font-medium text-sm text-gray-700 mb-2"
+            >
+              Name
+            </label>
+            <input
+              required
+              autoFocus
+              name="name"
+              type="text"
+              ref={register}
+              className="bg-white px-3 py-2 block w-full rounded border border-solid border-gray-300 outline-none"
+            />
+          </div>
 
-          {features ? (
-            <>
-              {availableFeatures.length > 0 && (
-                <div className="flex max-w-xl mb-2">
-                  <div className="relative flex-1">
-                    <select
-                      className="appearance-none bg-white px-3 py-2 block w-full rounded border border-solid border-gray-300 outline-none"
-                      value={selectedFeatureId}
-                      onChange={e => {
-                        const value = e.currentTarget.value
+          <div className="mb-3">
+            <label
+              htmlFor=""
+              className="block font-medium text-sm text-gray-700 mb-2"
+            >
+              Features
+            </label>
 
-                        if (value === 'all') {
-                          setSelectedFeatureIds(
-                            features.map(feature => feature.id)
-                          )
-                          return
-                        }
+            {features ? (
+              <>
+                {availableFeatures.length > 0 && (
+                  <div className="flex max-w-xl mb-2">
+                    <div className="relative flex-1">
+                      <select
+                        className="appearance-none bg-white px-3 py-2 block w-full rounded border border-solid border-gray-300 outline-none"
+                        value={selectedFeatureId}
+                        onChange={e => {
+                          const value = e.currentTarget.value
 
-                        setSelectedFeatureId(value)
-                      }}
-                    >
-                      <option value="">Select a feature</option>
-                      <option value="all">All features</option>
-                      {availableFeatures.map(feature => (
-                        <option value={feature.id} key={feature.id}>
-                          {feature.name}
-                        </option>
-                      ))}
-                    </select>
-                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                      <svg
-                        className="fill-current h-4 w-4"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
+                          if (value === 'all') {
+                            setSelectedFeatureIds(
+                              features.map(feature => feature.id)
+                            )
+                            return
+                          }
+
+                          setSelectedFeatureId(value)
+                        }}
                       >
-                        <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                      </svg>
+                        <option value="">Select a feature</option>
+                        <option value="all">All features</option>
+                        {availableFeatures.map(feature => (
+                          <option value={feature.id} key={feature.id}>
+                            {feature.name}
+                          </option>
+                        ))}
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg
+                          className="fill-current h-4 w-4"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                        >
+                          <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                        </svg>
+                      </div>
                     </div>
-                  </div>
 
-                  <button
-                    className="bg-indigo-100 text-indigo-700 px-3 py-2 rounded-r focus:outline-none text-xs font-medium uppercase"
-                    type="button"
-                    onClick={() =>
-                      setSelectedFeatureIds(ids => [...ids, selectedFeatureId])
-                    }
-                  >
-                    Add feature
-                  </button>
-                </div>
-              )}
-
-              {selectedFeatureIds.map((id, index) => {
-                const selectedFeature = features.find(
-                  feature => feature.id === id
-                )
-
-                if (!selectedFeature) return null
-
-                return (
-                  <div
-                    key={selectedFeature.id}
-                    className="relative rounded-lg bg-white shadow mb-2 md:max-w-xs md:inline-block md:w-full sm:mr-2"
-                  >
                     <button
+                      className="bg-indigo-100 text-indigo-700 px-3 py-2 rounded-r focus:outline-none text-xs font-medium uppercase"
                       type="button"
-                      className="absolute w-8 h-8 top-0 right-0 block text-sm hover:bg-gray-100 text-gray-500"
-                      onClick={() => unselectFeatureId(selectedFeature.id)}
+                      onClick={() =>
+                        setSelectedFeatureIds(ids => [
+                          ...ids,
+                          selectedFeatureId
+                        ])
+                      }
                     >
-                      <i className="fas fa-times"></i>
+                      Add feature
                     </button>
-
-                    <div className="p-6">
-                      <h3 className="text-lg font-medium">
-                        {selectedFeature.name}
-                      </h3>
-                      <p className="text-sm text-gray-700 truncate">
-                        {selectedFeature.description}
-                      </p>
-                    </div>
                   </div>
-                )
-              })}
-            </>
-          ) : (
-            <p>Loading features...</p>
-          )}
-        </div>
+                )}
 
-        <button className="shadow bg-indigo-500 text-white inline-block rounded-full py-2 px-4 text-xs font-medium uppercase mt-2">
-          Save survey
-        </button>
-      </form>
+                {selectedFeatureIds.map((id, index) => {
+                  const selectedFeature = features.find(
+                    feature => feature.id === id
+                  )
+
+                  if (!selectedFeature) return null
+
+                  return (
+                    <div
+                      key={selectedFeature.id}
+                      className="relative rounded-lg bg-white shadow mb-2 md:max-w-xs md:inline-block md:w-full sm:mr-2"
+                    >
+                      <button
+                        type="button"
+                        className="absolute w-8 h-8 top-0 right-0 block text-sm hover:bg-gray-100 text-gray-500"
+                        onClick={() => unselectFeatureId(selectedFeature.id)}
+                      >
+                        <i className="fas fa-times"></i>
+                      </button>
+
+                      <div className="p-6">
+                        <h3 className="text-lg font-medium">
+                          {selectedFeature.name}
+                        </h3>
+                        <p className="text-sm text-gray-700 truncate">
+                          {selectedFeature.description}
+                        </p>
+                      </div>
+                    </div>
+                  )
+                })}
+              </>
+            ) : (
+              <p>Loading features...</p>
+            )}
+          </div>
+
+          <button className="shadow bg-indigo-500 text-white inline-block rounded-full py-2 px-4 text-xs font-medium uppercase mt-2">
+            Save survey
+          </button>
+        </form>
+      </div>
     </>
   )
 }
