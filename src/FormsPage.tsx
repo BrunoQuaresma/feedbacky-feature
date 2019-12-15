5bbd0374940622a1c5257370027487493cf24590
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-12-14 23:25:07 -0300

    Refact forms page to use the new style

--- src/FormsPage.tsx
+++ src/FormsPage.tsx
@@ -4,7 +4,7 @@ import usePageView from './usePageView'
 import { Link } from 'react-router-dom'
 import useSWR from 'swr'
 import { getForms } from './api'
-import CardPlaceholder from './CardPlaceholder'
+import Loading from './Loading'
 
 const FormsPage: React.FC = () => {
   usePageView()
@@ -16,47 +16,47 @@ const FormsPage: React.FC = () => {
         <title>Forms - Feedbacky</title>
       </Helmet>
 
-      <div className="mb-4 flex items-center">
-        <h1 className="text-2xl">Forms</h1>
+      <section className="py-6 bg-white border-t-1 border-indigo-700 shadow mb-4">
+        <div className="container mx-auto px-2 md:flex">
+          <h1 className="leading-tight text-2xl font-medium">Forms</h1>
 
-        <div>
-          <Link
-            to="/forms/new"
-            className="ml-4 text-xs inline-block border border-solid border-indigo-500 text-indigo-700 hover:bg-indigo-100 rounded-full py-1 px-4 font-medium uppercase"
-          >
-            New form
-          </Link>
+          <div className="ml-auto mt-2 md:mt-0">
+            <Link
+              to="/forms/new"
+              className="text-xs inline-block border border-solid border-indigo-500 text-indigo-700 hover:bg-indigo-100 rounded-full py-1 px-4 font-medium uppercase"
+            >
+              New form
+            </Link>
+          </div>
         </div>
-      </div>
+      </section>
 
-      <div className="w-full mb-2">
+      <div className="container px-2 mx-auto">
         {forms ? (
           forms.map(form => (
-            <div
-              key={form.id}
-              className="p-6 py-4 rounded-lg bg-white shadow mb-2"
-            >
-              <Link to={`/forms/${form.id}`} className="text-lg font-medium">
-                {form.name}
-              </Link>
-              <p className="text-sm text-gray-700">
-                <i className="far fa-comments mr-1"></i>
-                {form.number_of_replies} replies
-              </p>
+            <div key={form.id} className="p-4 rounded-lg bg-white shadow mb-2">
+              <div className="flex items-baseline">
+                <div className="w-8 h-8 rounded-full bg-indigo-100 flex items-center justify-center mr-4">
+                  <i className="far fa-file-alt text-indigo-500"></i>
+                </div>
+                <div className="items-baseline md:flex md:w-full">
+                  <Link
+                    to={`/forms/${form.id}`}
+                    className="font-medium hover:text-indigo-500"
+                  >
+                    {form.name}
+                  </Link>
+
+                  <span className="block md:ml-auto text-gray-600 text-sm">
+                    <i className="far fa-comment text-indigo-500 mr-1"></i>
+                    {form.number_of_replies} replies
+                  </span>
+                </div>
+              </div>
             </div>
           ))
         ) : (
-          <>
-            <div className="mb-2">
-              <CardPlaceholder></CardPlaceholder>
-            </div>
-            <div className="mb-2">
-              <CardPlaceholder></CardPlaceholder>
-            </div>
-            <div className="mb-2">
-              <CardPlaceholder></CardPlaceholder>
-            </div>
-          </>
+          <Loading></Loading>
         )}
       </div>
     </>
