939ebbf459d1f226e27d3bbfe3297871000543d6
Bruno Quaresma <bruno_nonato_quaresma@hotmail.com>
2019-12-14 23:41:40 -0300

    Refact new feature page

--- src/NewFeaturePage.tsx
+++ src/NewFeaturePage.tsx
@@ -6,6 +6,7 @@ import 'easymde/dist/easymde.min.css'
 import { useHistory } from 'react-router'
 import Helmet from 'react-helmet'
 import usePageView from './usePageView'
+import { Link } from 'react-router-dom'
 
 type NewFeatureForm = {
   name: string
@@ -35,48 +36,59 @@ const NewFeaturePage: React.FC = () => {
         <title>New Feature. - Feedbacky</title>
       </Helmet>
 
-      <h1 className="uppercase text-sm font-medium text-gray-700 mb-4">
-        New Feature
-      </h1>
-
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-3 max-w-xl">
-          <label
-            htmlFor=""
-            className="block font-medium text-sm text-gray-700 mb-2"
-          >
-            Name
-          </label>
-          <input
-            required
-            autoFocus
-            name="name"
-            type="text"
-            ref={register}
-            className="bg-white px-3 py-2 block w-full rounded border border-solid border-gray-300 outline-none"
-          />
+      <section className="py-6 bg-white border-t-1 border-indigo-700 shadow mb-4">
+        <div className="container mx-auto px-2 md:flex">
+          <h1 className="leading-tight text-2xl font-medium">New feature</h1>
         </div>
+      </section>
 
-        <div className="mb-3">
-          <label
-            htmlFor=""
-            className="block font-medium text-sm text-gray-700 mb-2"
-          >
-            Description
-          </label>
+      <div className="container mx-auto px-2 py-4">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="mb-3 max-w-xl">
+            <label
+              htmlFor=""
+              className="block font-medium text-sm text-gray-700 mb-2"
+            >
+              Name
+            </label>
+            <input
+              required
+              autoFocus
+              name="name"
+              type="text"
+              ref={register}
+              className="bg-white px-3 py-2 block w-full rounded border border-solid border-gray-300 outline-none"
+            />
+          </div>
 
-          <SimpleMDE
-            onChange={value => setValue('description', value)}
-            options={{ spellChecker: false, status: false }}
-          />
-        </div>
+          <div className="mb-3">
+            <label
+              htmlFor=""
+              className="block font-medium text-sm text-gray-700 mb-2"
+            >
+              Description
+            </label>
 
-        <div className="flex justify-end">
-          <button className="shadow bg-indigo-500 text-white inline-block rounded-full py-2 px-4 text-xs font-medium uppercase mt-2">
-            Save feature
-          </button>
-        </div>
-      </form>
+            <SimpleMDE
+              onChange={value => setValue('description', value)}
+              options={{ spellChecker: false, status: false }}
+            />
+          </div>
+
+          <div className="flex justify-end">
+            <Link
+              to="/features"
+              className="border border-indigo-500 text-indigo-500 inline-block rounded-full py-2 px-4 text-xs font-medium uppercase mt-2 mr-2"
+            >
+              Cancel
+            </Link>
+
+            <button className="shadow bg-indigo-500 text-white inline-block rounded-full py-2 px-4 text-xs font-medium uppercase mt-2">
+              Save feature
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
